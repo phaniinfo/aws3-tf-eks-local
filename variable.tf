1a972863c8b0f4746cf1707958bce355a37e348f
phaniinfo <55450813+phaniinfo@users.noreply.github.com>
2022-09-06 13:37:55 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,16 @@
 variable "subnet_id_1" {
-  type = string
+  type    = string
   default = "subnet-0438ea35f865b5fb4"
- }
- 
- variable "subnet_id_2" {
-  type = string
+}
+variable "subnet_id_2" {
+  type    = string
   default = "subnet-0ea840422710040ae"
- }
+}
+variable "subnet_id_3" {
+  type    = string
+  default = "subnet-012fb4247812331b6"
+}
+variable "subnet_id_4" {
+  type    = string
+  default = "subnet-036a188b89e8bc3f2"
+}
